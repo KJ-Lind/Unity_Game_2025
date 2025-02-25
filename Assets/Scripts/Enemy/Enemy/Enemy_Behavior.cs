732b14110a4684fcc70937408c4977f90194dc10
Kaelin Lind <lind@esat-alumni.com>
2025-02-25 13:47:54 +0100

    Implementing Enemy Behaviour

--- Assets/Scripts/Enemy/Enemy/Enemy_Behavior.cs
+++ Assets/Scripts/Enemy/Enemy/Enemy_Behavior.cs
@@ -5,19 +5,59 @@ using UnityEngine;
 public class Enemy_Behavior : MonoBehaviour
 {
 
+    [SerializeField] private int enemyType_;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (enemyType_)
+        {
+            case 0:
+                Pattern_1();
+                break;
+
+            case 1:
+                Pattern_2();
+                break;
+
+            case 2:
+                Pattern_3();
+                break;
+
+            case 3:
+                Pattern_4();
+                break;
+        }
     }
+
+    void Pattern_1()
+    {
+
+    }
+
+    void Pattern_2()
+    {
+
+    }
+
+    void Pattern_3()
+    {
+
+    }
+
+    void Pattern_4()
+    {
+
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
